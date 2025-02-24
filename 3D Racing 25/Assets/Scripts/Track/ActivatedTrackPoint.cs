f4e564b1e237179226aaa60e3f6c8a53d063bb29
Murk999 <limonadik34rus@gmail.com>
2025-02-24 12:16:34 +0300

    add TrackPoint
    добавлены чекпоинты модель и скрипты

---
+++ 3D Racing 25/Assets/Scripts/Track/ActivatedTrackPoint.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ActivatedTrackPoint : TrackPoint
+{
+    [SerializeField] private GameObject hint;
+
+    private void Start()
+    {
+        hint.SetActive(false);
+    }
+    protected override void OnPassed()
+    {
+        hint.SetActive(false);
+    }
+    protected override void OnAssignAsTarget()
+    {
+        hint.SetActive(true);
+    }
+}
---
+++ 3D Racing 25/Assets/Scripts/Track/TrackpointCircuit.cs
@@ -0,0 +1,70 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum TrackType
+{
+    Circular,
+    Sprint
+}
+
+public class TrackPointCircuit : MonoBehaviour
+{
+    public event UnityAction<TrackPoint> TrackPointTriggered;
+    public event UnityAction<int> LapCompleted;
+
+    [SerializeField] private TrackType type;
+    public TrackType Type => type;
+
+    private TrackPoint[] points;
+
+    private int lapsCompleted = -1;
+
+    private void Awake()
+    {
+        BuildCircuit();
+    }
+
+    private void Start()
+    {
+        for(int i = 0; i < points.Length; i++)
+        {
+            points[i].Triggered += OnTrackPointTriggered;
+        }
+        points[0].AssignAsTarget();
+    }
+    [ContextMenu(nameof(BuildCircuit))]
+    private void BuildCircuit()
+    {
+        points = TrackCircuitBuilder.Build(transform, type);
+    }
+
+    private void OnTrackPointTriggered(TrackPoint trackPoint)
+    {
+        if (trackPoint.IsTarget == false) return;
+        trackPoint.Passed();
+        trackPoint.Next?.AssignAsTarget();
+
+        TrackPointTriggered?.Invoke(trackPoint);
+
+        if (trackPoint.IsLast == true)
+        {
+            if(type == TrackType.Sprint)
+            {
+                lapsCompleted++;
+                if (type == TrackType.Sprint)
+                {
+                    LapCompleted?.Invoke(lapsCompleted);
+                }
+                
+                if(type== TrackType.Circular)
+                {
+                    if(lapsCompleted > 0)
+                    {
+                        LapCompleted?.Invoke(lapsCompleted);
+                    }
+                }
+            }
+        }
+    }
+}
